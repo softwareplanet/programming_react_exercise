4183545d6848bff6b4235e3551406851725f8127
Nazarii Besiedovskyi <nazariibe@softwareplanet.uk.com>
2021-03-12 10:34:18 +0200

    Implement List component

--- src/components/List.js
+++ src/components/List.js
@@ -2,10 +2,33 @@ import React from 'react';
 import './Components.css';
 
 class List extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.sortedWords = this.sortWords(props.words);
+    }
+
+    sortWords(words) {
+        // We dont want to modify array in place
+        words = [...words];
+
+        return words.sort((a, b) => {
+            a = a.toLowerCase();
+            b = b.toLowerCase();
+            if (a == b) return 0;
+            if (a > b) return 1;
+            return -1;
+        });
+    }
+
     render() {
         return (
             <div className="List">
-                <span>Placeholder</span>
+                <ol className="WordsList">
+                    {this.sortedWords.map(word => {
+                        return <li key={word}><p className="Word">{word}</p></li>;
+                    })}
+                </ol>
             </div>
           );
     }
